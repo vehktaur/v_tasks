b7b7bc87161fa3c14350bee3e0d38dd86873e86b
vehktaur <victorakhihiero@gmail.com>
2025-02-24 10:53:10 +0100

    Wrap DroppableColumns in Suspense for improved loading handling; adjust main component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import DroppableColumns from '@/components/home/droppable-columns';
+import { Suspense } from 'react';
 
 const Home = async (props: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
@@ -7,8 +8,10 @@ const Home = async (props: {
   // Get search from search params and pass to columns
   const search = searchParams.search?.toString() || '';
   return (
-    <main className='padding-inline ~pb-5/8 grid grid-cols-1 items-start justify-center justify-items-center gap-x-4 gap-y-8 overflow-x-hidden md:grid-cols-2 xl:grid-cols-3'>
-      <DroppableColumns search={search}/>
+    <main className='padding-inline grid grid-cols-1 items-start justify-center justify-items-center gap-x-4 gap-y-8 overflow-x-hidden ~pb-5/8 md:grid-cols-2 xl:grid-cols-3'>
+      <Suspense key={search}>
+        <DroppableColumns search={search} />
+      </Suspense>
     </main>
   );
 };
